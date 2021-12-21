62c366d3ad12951ceb25eb5baf9660de1a546d55
Anton Kruglyakov <antonkrug.lav@gmail.com>
2021-12-21 12:47:10 +0100

    le prime prove

---
+++ PrUni/src/main/java/controllers/ControllerRest.java
@@ -0,0 +1,59 @@
+package controllers;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerRest {
+
+	String url = "https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/search/tweets.json?q=calcio&count=2";
+	
+	@GetMapping("/hello")
+	
+	public String helloWorld(@RequestParam(name = "par" ) String par) 
+	{
+		return "Ciao Mondo " + par;
+	}
+	
+	
+	@GetMapping("/tweet")
+	public String cercaTweet() 
+	{
+		String data = "";
+		try
+		{
+			URLConnection openConnection = new URL(url).openConnection();
+			InputStream in = openConnection.getInputStream();
+			
+			
+			String line = "";
+			 
+			try {
+			   InputStreamReader inR = new InputStreamReader( in );
+			   BufferedReader buf = new BufferedReader( inR );
+			  
+			   while ( ( line = buf.readLine() ) != null ) {
+				   data+= line;
+			   }
+			} finally {
+			  in.close();
+			}
+		
+		}catch(Exception errore)
+		{
+			errore.printStackTrace();
+		}
+		
+		
+		
+		return data;
+	}
+	
+}
