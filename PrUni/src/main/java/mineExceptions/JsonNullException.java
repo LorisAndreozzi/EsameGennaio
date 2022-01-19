31256cb272425cf0d2b9d2da667a0a7281e28e37
Anton Kruglyakov <antonkrug.lav@gmail.com>
2022-01-19 20:22:37 +0100

    RM3

--- PrUni/src/main/java/mineExceptions/JsonNullException.java
+++
@@ -1,9 +0,0 @@
-package mineExceptions;
-
-public class JsonNullException extends Exception{
-
-	  public JsonNullException() { super(); }
-	  public JsonNullException(String message) { super(message); }
-	  public JsonNullException(String message, Throwable cause) { super(message, cause); }
-	  public JsonNullException(Throwable cause) { super(cause); }
-}
